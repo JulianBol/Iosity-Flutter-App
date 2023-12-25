46aadd1b75447f2d93da24fd3ec369facc5006f5
Julian Bolaños <julian.bolanos.guerrero@gmail.com>
2023-12-25 15:58:39 -0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iosity/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,12 +8,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    
+    return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
       title: 'Iosity',
-      home: Scaffold(
-        body: Center(child: Text('Iosity'))
-      )
+      
+      initialRoute: 'login',
+      routes: {
+        'login': (_) => const LoginScreen(),
+        'home': (_) => const HomeScreen()
+      }
     );
   }
 }
