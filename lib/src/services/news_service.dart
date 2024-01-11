6b208a24f749bbfeb5dbb20c2613d329da57daac
Julian Bolaños <julian.bolanos.guerrero@gmail.com>
2024-01-10 21:51:46 -0600

    category model created

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:iosity/src/models/category_model.dart';
 import 'package:iosity/src/models/news_models.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,6 +15,15 @@ const _API_KEY = 'a1492f40ffec4f0286854aed71a16691';
 class NewsService extends ChangeNotifier {
 
   List <Article> headLines = [];
+  List <Category> categories = [
+    Category(icon: FontAwesomeIcons.building, name: 'Business'),
+    Category(icon: FontAwesomeIcons.tv, name: 'Entertainment'),
+    Category(icon: FontAwesomeIcons.addressCard, name: 'General'),
+    Category(icon: FontAwesomeIcons.headSideVirus, name: 'Health'),
+    Category(icon: FontAwesomeIcons.vials, name: 'Science'),
+    Category(icon: FontAwesomeIcons.futbol, name: 'Sports'),
+    Category(icon: FontAwesomeIcons.memory, name: 'Technology')
+  ];
 
   NewsService(){
     getTopHeadLines();
