4fe17387e5b72bb3d15a598d53b71a23a615e5fa
Julian Bolaños <julian.bolanos.guerrero@gmail.com>
2024-01-11 20:45:52 -0600

    service update to obtain news by category

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -16,18 +16,48 @@ class NewsService extends ChangeNotifier {
 
   List <Article> headLines = [];
   List <Category> categories = [
-    Category(icon: FontAwesomeIcons.building, name: 'Business'),
-    Category(icon: FontAwesomeIcons.tv, name: 'Entertainment'),
-    Category(icon: FontAwesomeIcons.addressCard, name: 'General'),
-    Category(icon: FontAwesomeIcons.headSideVirus, name: 'Health'),
-    Category(icon: FontAwesomeIcons.vials, name: 'Science'),
-    Category(icon: FontAwesomeIcons.futbol, name: 'Sports'),
-    Category(icon: FontAwesomeIcons.memory, name: 'Technology')
+    Category(icon: FontAwesomeIcons.building, name: 'business'),
+    Category(icon: FontAwesomeIcons.tv, name: 'entertainment'),
+    Category(icon: FontAwesomeIcons.addressCard, name: 'general'),
+    Category(icon: FontAwesomeIcons.headSideVirus, name: 'health'),
+    Category(icon: FontAwesomeIcons.vials, name: 'science'),
+    Category(icon: FontAwesomeIcons.futbol, name: 'sports'),
+    Category(icon: FontAwesomeIcons.memory, name: 'technology')
   ];
 
+
+  Map <String, List <Article>> categoryArticles = {};
+  String _selectedCategory = 'business';
+
+
   NewsService(){
+
+    // Hacemos la petición para obtener 
+    // las Noticias en Tendencia.
     getTopHeadLines();
+
+    // Inicializamos el Mapa de < categoryArticles >
+    categories.forEach((element) {
+      categoryArticles[element.name] = <Article> [];
+    });
+
+    // Hacemos la petición para obtener 
+    // las Noticias de la categoria por
+    // defecto 'business'.
+    getHeadLinesByCategory(_selectedCategory);
   }
+
+
+  String get selectedCategory => _selectedCategory;
+
+  set selectedCategory(String value) {
+    _selectedCategory = value;
+    getHeadLinesByCategory(value);
+
+    notifyListeners();
+  }
+
+  List<Article> get getArticlesCategory => (categoryArticles[_selectedCategory]!);
 
   void getTopHeadLines() async {
 
@@ -42,6 +72,27 @@ class NewsService extends ChangeNotifier {
 
     final NewsResponse newsResponse = newsResponseFromJson(response.body);
     headLines.addAll(newsResponse.articles);
+
+    notifyListeners();
+  }
+
+   getHeadLinesByCategory(String category) async {
+
+    if(categoryArticles[category]!.isNotEmpty) {
+      return categoryArticles[category];
+    }
+
+    final Uri url = Uri(
+      scheme: 'https',
+      host: _HOST,
+      path: _PATH,
+      queryParameters: {'country': _COUNTRY, 'apiKey': _API_KEY, 'category': category}
+    );
+
+    final response = await http.get(url);
+
+    final NewsResponse newsResponse = newsResponseFromJson(response.body);
+    categoryArticles[category]!.addAll(newsResponse.articles);
 
     notifyListeners();
   }
